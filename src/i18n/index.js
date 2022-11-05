47881eddfe9868fbb9ec38dc4646060b760e80eb
milad-emami <milad.emami1025@gmail.com>
2022-11-05 08:09:16 +0330

    feat: add persian lang

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -9,10 +9,12 @@ import deMessages from './messages/de.json';
 import ruMessages from './messages/ru.json';
 import hiMessages from './messages/hi.json';
 import frCAMessages from './messages/fr_CA.json';
+import faIRMessages from './messages/fa_IR.json';
 // no need to import en messages-- they are in the defaultMessage field
 
 const messages = {
   ar: arMessages,
+  fa: faIRMessages,
   'es-419': es419Messages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
